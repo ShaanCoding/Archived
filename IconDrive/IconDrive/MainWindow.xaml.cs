94202e035b4eb4b965e8cdc901f89a93fd873e7a
Shaan Khan <shaankhan101@gmail.com>
2019-11-12 21:28:03 +1100

    Inital Commit
    Inital Commit Of Icon Drive, A simple application that installs a custom icon onto your USB, this was a challenge to further learn WPF

---
+++ IconDrive/IconDrive/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IconDrive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string selectedIconString = "";
+        public static DriveInfo[] driveArray;
+        public static string selectedDriveString = "";
+        public static string nameOfDrive = "";
+        bool readyToInstall = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Initalises drive label & selected drive
+            driveArray = DriveInfo.GetDrives();
+            if (driveArray.Length >= 2)
+            {
+                selectedDriveString = driveArray[1].Name;
+                nameOfDrive = driveArray[1].VolumeLabel;
+            }
+            else
+            {
+                selectedDriveString = driveArray[0].Name;
+                nameOfDrive = driveArray[0].VolumeLabel;
+            }
+            SelectedDriveLabel.Content = nameOfDrive + " (" + selectedDriveString + ")";
+        }
+
+        private void IconSelector_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog selectIcon = new OpenFileDialog();
+            selectIcon.Filter = "ICO|*.ico";
+            if(selectIcon.ShowDialog() == true)
+            {
+                selectedIconString = selectIcon.FileName;
+                iconSelector.Content = System.IO.Path.GetFileName(selectIcon.FileName);
+                readyToInstall = true;
+                installButton.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF31C4FF"));
+            }
+        }
+
+        private void SelectDrive_Click(object sender, RoutedEventArgs e)
+        {
+            selectDriveDialog selectDriveDialog = new selectDriveDialog();
+            selectDriveDialog.Owner = this;
+            selectDriveDialog.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            if (selectDriveDialog.ShowDialog() == true)
+            {
+                selectedDriveString = selectDriveDialog.Answer;
+                nameOfDrive = selectDriveDialog.Answer2;
+                SelectedDriveLabel.Content = nameOfDrive + " (" + selectedDriveString + ")";
+            }
+        }
+
+        private void InstallButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //replace this with styling
+                if (readyToInstall == true)
+                {
+                    //Writes autorun.inf & places logo (both are set to hidden in windows)
+
+                    //Deletes original file prior to writing
+                    if (File.Exists(selectedDriveString + "autorun.inf"))
+                    {
+                        File.Delete(selectedDriveString + "autorun.inf");
+                    }
+                    if (File.Exists(selectedDriveString + "icon.ico"))
+                    {
+                        File.Delete(selectedDriveString + "icon.ico");
+                    }
+
+                    //Copys files over
+                    File.Copy(selectedIconString, selectedDriveString + "icon.ico");
+
+                    using (Stream s = File.Open(selectedDriveString + "autorun.inf", FileMode.CreateNew))
+                    {
+                        using (StreamWriter sw = new StreamWriter(s))
+                        {
+                            sw.WriteLine("[autorun]");
+                            sw.WriteLine("icon=icon.ico");
+                            sw.Close();
+                            s.Close();
+                        }
+                    }
+
+                    //Makes hidden
+                    File.SetAttributes(selectedDriveString + "icon.ico", File.GetAttributes(selectedDriveString + "icon.ico") | FileAttributes.Hidden);
+                    File.SetAttributes(selectedDriveString + "autorun.inf", File.GetAttributes(selectedDriveString + "autorun.inf") | FileAttributes.Hidden);
+                    MessageBox.Show("Application Sucessfully Installed!");
+                }
+                else if (readyToInstall == false)
+                {
+                    MessageBox.Show("Not Ready To Install, Please Select Icon");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Icon Installation Failed!");
+            }
+        }
+    }
+}
---
+++ IconDrive/IconDrive/selectDriveDialog.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IconDrive
+{
+    /// <summary>
+    /// Interaction logic for selectDriveDialog.xaml
+    /// </summary>
+    public partial class selectDriveDialog : Window
+    {
+        public static DriveInfo[] driveArray;
+        public selectDriveDialog()
+        {
+            InitializeComponent();
+            driveArray = DriveInfo.GetDrives();
+            for(int i = 0; i < driveArray.Length; i++)
+            {
+                listBoxAnswer.Items.Add(driveArray[i].Name);
+            }
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            if(driveArray.Length >= 2)
+            {
+                listBoxAnswer.SelectedIndex = 1;
+                listBoxAnswer.UpdateLayout();
+            }
+            else
+            {
+                listBoxAnswer.SelectedIndex = 0;
+                listBoxAnswer.UpdateLayout();
+            }
+            /*
+            txtAnswer.SelectAll();
+            txtAnswer.Focus();
+            */
+        }
+        public string Answer
+        {
+            get { return listBoxAnswer.SelectedItem.ToString(); }
+        }
+
+        public string Answer2
+        {
+            get { return driveArray[listBoxAnswer.SelectedIndex].VolumeLabel; }
+        }
+        private void ListBoxAnswer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void BtnDialogOk_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
