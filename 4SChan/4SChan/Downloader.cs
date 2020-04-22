0b6d39f33da67d75987dd03a832d613bda9c8ec3
Shaan Khan <shaankhan101@gmail.com>
2020-04-22 20:29:13 +1000

    Added skeleton of program
    Added basic skeleton of program currently using:
    https://github.com/Frioo/ChanDownloader/blob/master/ChanDownloader/Downloader.cs as current API for MVP downloader, will replace once basis of program is setup

---
+++ 4SChan/4SChan/Downloader.cs
@@ -0,0 +1,140 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4SChan
+{
+    public class Downloader
+    {
+        public WebClient WebClient = new WebClient();
+        public int CurrentFileNumber = 1;
+        private Thread _thread;
+        private const string api_url = @"https://a.4cdn.org/";
+        private const string api_img_url = @"https://i.4cdn.org/";
+
+        public async Task<Thread> LoadThread(string url)
+        {
+            Utils.Log("LoadThread: extracting info");
+            //turn http://boards.4chan.org/<board>/thread/<id>/<sometimes_title> into <board>/thread/<id>
+            var endpoint = string.Join("/", url.Remove(0, url.LastIndexOf('.')).Split('/'), 1, 3);
+            var apiUrl = $"{api_url}{endpoint}.json";
+
+            Utils.Log($"api url: {apiUrl}");
+            var json = await WebClient.DownloadStringTaskAsync($"{api_url}{endpoint}.json");
+
+            if (string.IsNullOrEmpty(json))
+            {
+                // request error
+                Utils.Log("LoadThread: web request returned null");
+                return null;
+            }
+
+            var posts = JObject.Parse(json)["posts"].ToObject<JArray>();
+            if (posts == null)
+            {
+                // parsing error
+                Utils.Log($"LoadThread: error parsing response\njson:\n{json}");
+                return null;
+            }
+
+            var id = posts[0]["no"].ToString();
+            var semantic = posts[0]["semantic_url"].ToString();
+            var subject = posts[0]["sub"] == null ? semantic : posts[0]["sub"].ToString();
+            var files = new List<File>();
+
+            for (int i = 0; i < posts.Count; i++)
+            {
+                if (posts[i]["filename"] != null)
+                {
+                    var filename = posts[i]["filename"].ToString() + posts[i]["ext"].ToString();
+                    var size = int.Parse(posts[i]["fsize"].ToString());
+                    var renamed = posts[i]["tim"].ToString() + posts[i]["ext"].ToString();
+                    var uri = $"{api_img_url}{endpoint.Split('/').First()}/{renamed}";
+                    files.Add(new File(filename, renamed, size, uri));
+                }
+            }
+
+            var thread = new Thread(url, id, subject, semantic, files);
+            this._thread = thread;
+            Utils.Log($"Loaded thread: {id} - {subject} ({files.Count} files)");
+
+            return thread;
+        }
+
+        public async Task DownloadFiles(Thread thread, string path)
+        {
+            var files = thread.Files;
+            for (int i = 0; i < files.Count; i++)
+            {
+                this.CurrentFileNumber = i + 1;
+                var current = files[i];
+                var filename = $"{path}\\{current.OriginalFileName}";
+                var uri = $"{current.Url}";
+                Utils.Log($"downloading {uri} to {filename}");
+                await WebClient.DownloadFileTaskAsync(uri, filename);
+            }
+            Utils.Log($"downloaded {files.Count} files to {path}");
+        }
+
+        public async Task DownloadFiles(List<File> files, string path)
+        {
+            for (int i = 0; i < files.Count; i++)
+            {
+                this.CurrentFileNumber = i + 1;
+                var current = files[i];
+                var filename = $"{path}\\{current.OriginalFileName}";
+                var uri = $"{current.Url}";
+                Utils.Log($"downloading {uri} to {filename}");
+                await WebClient.DownloadFileTaskAsync(uri, filename);
+            }
+            Utils.Log($"downloaded {files.Count} files to {path}");
+        }
+    }
+
+    public class Thread
+    {
+        public string Url { get; private set; }
+        public string Id { get; private set; }
+        public string Subject { get; private set; }
+        public string SemanticSubject { get; private set; }
+        public List<File> Files { get; private set; }
+
+        public Thread(string url, string id, string subject, string semantic, List<File> files)
+        {
+            this.Url = url;
+            Id = id;
+            Subject = subject;
+            SemanticSubject = semantic;
+            Files = files;
+        }
+    }
+
+    public class File
+    {
+        public string OriginalFileName { get; private set; }
+        public string FileName { get; private set; }
+        public int FileSize { get; private set; }
+        public string Url { get; private set; }
+
+        public File(string originalName, string filename, int filesize, string url)
+        {
+            this.OriginalFileName = originalName;
+            this.FileName = filename;
+            this.FileSize = filesize;
+            this.Url = url;
+        }
+    }
+
+    public static class Utils
+    {
+        public static void Log(string text)
+        {
+            Debug.WriteLine($"ChanDownloader-log: {text}");
+        }
+    }
+}
---
+++ 4SChan/4SChan/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _4SChan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BackgroundWorker backgroundWorker = new BackgroundWorker();
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+            */
+        }
+
+        private static void ReadOnlyButton()
+        {
+            /*
+            if (startStopButtonBool == true)
+            {
+                if (isFolderSelected == true)
+                {
+                    FindMaxPages();
+                    backgroundWorker.RunWorkerAsync();
+                    startStopButtonBool = false;
+                    StartStopButton.Content = "Stop";
+                }
+                else
+                {
+                    MessageBox.Show("Error: Cannot start until a output folder is selected.");
+                }
+            }
+            else
+            {
+                if (backgroundWorker.IsBusy)
+                {
+                    backgroundWorker.CancelAsync();
+                }
+                startStopButtonBool = true;
+                StartStopButton.Content = "Start";
+            }
+            */
+        }
+
+        public static async void TestFunction()
+        {
+            /*
+            var downloader = new Downloader();                        // create new ChanDownloader instance
+
+            var thread = await downloader.LoadThread("Thread URL");   // load thread
+
+            var id = thread.Id;                                        // thread id
+            var subject = thread.Subject;                             // get thread subject (original)
+            var safeName = thread.SemanticSubject;                    // get thread subject in safe format
+            var files = thread.Files;                                 // get the file list
+
+            
+             //the webclient is exposed so you can hook up your event callbacks
+             //you can get the current downloaded file index from downloader.CurrentFileNumber
+
+            downloader.WebClient.DownloadFileCompleted += "YOUR CALLBACK";
+            await downloader.DownloadFiles(thread, "YOUR PATH");  // download the files to the specified directory
+            */
+        }
+
+        private void BackgroundWorkerDownloadingImages(object sender, DoWorkEventArgs e)
+        {
+            /*
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            for (int i = 1; i <= numberOfComics; i++)
+            {
+                while (isPausedBool == true)
+                {
+                    //Empty loop to pause downloading
+                }
+
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    try
+                    {
+                        string[] imageURLDataString = ImageURLData(i);
+                        using (WebClient client = new WebClient())
+                        {
+                            client.DownloadFile(new Uri(imageURLDataString[0]), string.Format(@"{0}\{1}.png", downloadDirectory, imageURLDataString[1]));
+
+                            dataGrid.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal,
+                            new Action(delegate ()
+                            {
+                                DataTableAdd(i, numberOfComics, imageURLDataString[1], Convert.ToInt64(client.ResponseHeaders["Content-Length"]).ToString());
+                            }));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error has occured, refer to: " + ex);
+                    }
+
+                }
+                int returnProgressPercent = (int)Math.Ceiling((decimal)(i + 1) / numberOfComics * 100);
+                worker.ReportProgress(returnProgressPercent);
+            }
+            */
+        }
+
+        private static void BackgroundWorkerDownloadingImages_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Operation Cancelled");
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show("Error in Process :" + e.Error);
+            }
+            else
+            {
+                MessageBox.Show("Success! The task has finished successfully.");
+            }
+        }
+
+        private void BackgroundWorkerDownloadingImages_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //ProgressBar.Value = e.ProgressPercentage;
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsMenu settingsMenu = new SettingsMenu();
+            settingsMenu.ShowDialog();
+        }
+    }
+}
