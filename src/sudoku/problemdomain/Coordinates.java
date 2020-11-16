66341f5668fcd252ddcbc8316c5ca2fb770bd12c
Shaan Khan <shaankhan101@gmail.com>
2020-11-16 20:18:58 +1100

    Finished program
    Finished the program according to the youtube tutorial

--- src/sudoku/problemdomain/Coordinates.java
+++ src/sudoku/problemdomain/Coordinates.java
@@ -2,43 +2,35 @@ package sudoku.problemdomain;
 
 import java.util.Objects;
 
+/**
+ * Convenience class for storing the location of a given tile in the Sudoku puzzle in a Hashmap.
+ */
 public class Coordinates {
     private final int x;
     private final int y;
 
-    public Coordinates(int x, int y)
-    {
+    public Coordinates(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public int getX()
-    {
+    public int getX() {
         return x;
     }
 
-    public int getY()
-    {
+    public int getY() {
         return y;
     }
 
     @Override
     public boolean equals(Object o) {
-        if(this == o){
-            return true;
-        }
-        if(o == null | getClass() != o.getClass()) {
-            return false;
-        }
-
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Coordinates that = (Coordinates) o;
         return x == that.x &&
                 y == that.y;
     }
 
-    /*
-        Hashcode function
-     */
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
