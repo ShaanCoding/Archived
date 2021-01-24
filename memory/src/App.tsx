0fe60b26cdc779a40ff616c92a7f21f9a5fee0f6
Shaan <shaankhan101@gmail.com>
2021-01-25 06:07:38 +1100

    Initial commit
    Initial commit of core files of react project

---
+++ memory/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <AddCardBox />
+
+      <CardList />
+    </>
+  );
+}
+
+export default App;
