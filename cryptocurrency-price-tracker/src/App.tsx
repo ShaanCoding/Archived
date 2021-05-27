182c7428b3b4252bbe0b4f36b2724f34280505eb
Shaan <shaankhan101@gmail.com>
2021-05-28 08:45:34 +1000

    Implemented API

--- cryptocurrency-price-tracker/src/App.tsx
+++ cryptocurrency-price-tracker/src/App.tsx
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 import './App.css';
+import Coin from './Components/Coin';
 
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=AUD&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
 
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=AUD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -18,13 +21,28 @@ function App() {
       alert(error)})
   }, []);
 
+  const handleChange = (e: any) => {
+    setSearch(e.target.value)
+  };
+
+  // Filter coin
+
+  const filteredCoins = coins.filter((coin: any) => 
+    coin.name.toLowerCase().includes(search.toLowerCase())   
+  );
+
   return (
     <>
       <form>
-          <input type="text" placeholder="Search" className="coin-input"/>
+          <input type="text" placeholder="Search" className="coin-input" onChange={handleChange}/>
       </form>
 
       {/* Map all coins here */}
+      {filteredCoins.map((coin: any) => {
+        return (
+          <Coin key={coin.id} coinName={coin.name} coinImage={coin.image} coinSymbol={coin.symbol} coinVolume={coin.market_cap} coinPrice={coin.current_price}/>          
+          );
+      })}
     </>
   );
 }
--- cryptocurrency-price-tracker/src/Components/Coin.tsx
+++ cryptocurrency-price-tracker/src/Components/Coin.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Coin: React.FC<{}> = () => {
+const Coin: React.FC<{coinImage: string, coinName: string, coinSymbol: string, coinPrice: string, coinVolume: string}> = (props) => {
     return (
         <div>
-            
+            <img src={props.coinImage} alt="Coin"/>
+            <h1>{props.coinName}</h1>
+            <p>{props.coinSymbol}</p>
+            <div>
+                <h2>{props.coinPrice}</h2>
+                <p>{props.coinVolume}</p>
+            </div>
         </div>
     )
 }
